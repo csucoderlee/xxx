1c0e75ed4a86e7085ec2e3f4107ab134d6475f97
lixiang <csucoderlee@gmail.com>
2017-11-13 23:07:49 +0800

    暂时提交

---
+++ xxx-index/src/main/java/org/csu/coderlee/entity/Revert.java
@@ -0,0 +1,10 @@
+package org.csu.coderlee.entity;
+
+/**
+ * @author by bixi.lx
+ * @created on 2017 11 13 22:48
+ */
+public class Revert {
+
+    private Long topicId;
+}
---
+++ xxx-index/src/main/java/org/csu/coderlee/entity/Topic.java
@@ -0,0 +1,13 @@
+package org.csu.coderlee.entity;
+
+/**
+ * @author by bixi.lx
+ * @created on 2017 11 13 22:31
+ */
+public class Topic {
+
+    private String theme;
+    private Long createrId;
+    private Long created;
+    private Long modified;
+}
